537001278512e7f215fe093845de1dff3e322494
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-01-29 20:05:32 +0000

    Initial commit

---
+++ gig_soft/infographics/app/installation/save_uploaded_image.php
@@ -0,0 +1,12 @@
+﻿<?php
+if (isset ( $GLOBALS ["HTTP_RAW_POST_DATA"] )) {
+$file = $GLOBALS ["HTTP_RAW_POST_DATA"];
+
+$uniqueStamp = uniqid();
+$filename = $uniqueStamp.".png";
+$filename = "uploaded_image/".$filename;
+$fp = fopen ( $filename, 'wb' );
+fwrite ( $fp, $file );
+fclose ( $fp );
+echo "imagepath=".$filename;
+}
